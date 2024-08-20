f4a41b19b7a1b51ca08fc409f22317c1bb55ef48
George Zubrienko <gzu@ecco.com>
2024-08-20 17:03:59 +0200

    Add support for XML content wrap

--- src/Controllers/IngestionController.cs
+++ src/Controllers/IngestionController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Text.Json;
+using System.Xml;
+using Arcane.Ingestion.Extensions;
 using Arcane.Ingestion.Services.Base;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -26,10 +28,21 @@ namespace Arcane.Ingestion.Controllers
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public ObjectResult Ingest([FromBody] JsonDocument record, string source)
         {
-            this.logger.LogDebug("Received record for {source}", source);
+            this.logger.LogDebug("Received JSON record for {source}", source);
 
             this.jsonService.Ingest(source, record);
             return this.Accepted();
         }
+        
+        [HttpPost("xml/{source}")]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public ObjectResult Ingest([FromBody] XmlDocument record, string source)
+        {
+            this.logger.LogDebug("Received XML record for {source}", source);
+
+            this.jsonService.Ingest(source, record.WrapXml());
+            return this.Accepted();
+        }        
     }
 }
---
+++ src/Extensions/IngestionExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+
+namespace Arcane.Ingestion.Extensions;
+
+/// <summary>
+/// Utility methods for pre-processing payloads for the ingestion service.
+/// </summary>
+public static class IngestionExtensions
+{
+    /// <summary>
+    /// Wraps XML input as base64-encoded content data and returns a JsonDocument.
+    /// </summary>
+    /// <param name="xml"></param>
+    /// <returns></returns>
+    public static JsonDocument WrapXml(this XmlDocument xml)
+    {
+        var contentBytes = Encoding.Unicode.GetBytes(xml.OuterXml);
+        return JsonSerializer.SerializeToDocument(new
+        {
+            ContentData = Convert.ToBase64String(contentBytes),
+            ContentMd5 = Convert.ToBase64String(MD5.HashData(contentBytes))
+        });
+    }
+}
